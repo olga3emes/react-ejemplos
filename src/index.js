bb4aab5c33a166d04d3b35a5e217dd1317adf1ac
olga3emes@gmail.com <olga3emes@gmail.com>
2023-07-04 12:59:53 +0200

    Ejemplo 002

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-
-const jsx = <h1>Hello JSX!</h1>;
+const jsx_valid = 
+<>
+<p>Paragraph 1</p>
+<p>Paragraph 2</p>
+<p>Paragraph 3</p>
+</>;
+const jsx_valid2 = 
+<div>
+<p>Paragraph 4</p>
+<p>Paragraph 5</p>
+<p>Paragraph 6</p>
+</div>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  jsx
+root.render([
+  jsx_valid,
+  jsx_valid2]
 );
 
