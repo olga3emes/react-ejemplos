d97432d13c95e37b5f4e82cf2c8c95e6e08a6380
olga3emes@gmail.com <olga3emes@gmail.com>
2023-07-04 13:04:57 +0200

    Ejemplo 003

--- src/index.js
+++ src/index.js
@@ -5,17 +5,15 @@ const jsx_valid =
 <>
 <p>Paragraph 1</p>
 <p>Paragraph 2</p>
+{/* Esto es un comentario
+y puede tener
+varias líneas */}
 <p>Paragraph 3</p>
 </>;
-const jsx_valid2 = 
-<div>
-<p>Paragraph 4</p>
-<p>Paragraph 5</p>
-<p>Paragraph 6</p>
-</div>;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render([
-  jsx_valid,
-  jsx_valid2]
+  jsx_valid]
 );
 
