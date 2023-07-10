82777a5be657c3b59c7b0687890f0d8bb8d41f6d
olga3emes@gmail.com <olga3emes@gmail.com>
2023-07-10 14:12:34 +0200

    Ejemplo 013

--- src/components/shop/Items.js
+++ src/components/shop/Items.js
@@ -1,12 +1,12 @@
-// import PropTypes from 'prop-types';
+// // // import PropTypes from 'prop-types';
 
-// export default function Items(props) {
-//     // Items.defaultProps = {
-//     //     quantity: 0
-//     // }
-//     Items.propTypes={handleClick: PropTypes.func.isRequired}
-//     return <h2>El total de items en el carrito es de: {props.quantity}</h2>
-// };
+// // // export default function Items(props) {
+// // //     // Items.defaultProps = {
+// // //     //     quantity: 0
+// // //     // }
+// // //     Items.propTypes={handleClick: PropTypes.func.isRequired}
+// // //     return <h2>El total de items en el carrito es de: {props.quantity}</h2>
+// // // };
 
 import PropTypes from 'prop-types';
 import React from 'react';
