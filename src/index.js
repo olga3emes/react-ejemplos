59d02d4cba636edb713b96a4717c3538e3045da6
olga3emes@gmail.com <olga3emes@gmail.com>
2023-07-10 13:36:02 +0200

    Ejemplo 012

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ToDo from './components/tareas/ToDo';
+import ToDo from './components/task/ToDo';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root'));
